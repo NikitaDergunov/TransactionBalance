64b17122a63014a2f0522f9a8414639c910b6eaf
Nikita Dergunov <piculiwar@gmail.com>
2021-10-26 15:20:29 +0300

    Added USD exchange functionality

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,9 +1,11 @@
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Money {
     private String name;
     private BigDecimal amount;
-    //private BigDecimal exchange;
+    private BigDecimal exchange = null;
+    private boolean exchangeset = false;
     public Money(String name){
         this.name = name;
         amount = BigDecimal.valueOf(0);
@@ -13,7 +15,8 @@ public class Money {
         this.amount = amount;
     }
     public String getString(){
-        return name + " " + amount.doubleValue();
+        if(!exchangeset) return name + " " + amount.doubleValue();
+        else return name + " " + amount.doubleValue() + " (USD " + amount.multiply(exchange).doubleValue()+")";
     }
     public void add(BigDecimal n){
 
@@ -25,5 +28,9 @@ public class Money {
     }
     public BigDecimal getAmount(){
         return amount;
+    }
+    public void setExchange(BigDecimal exchange){
+        exchangeset = true;
+        this.exchange = exchange.divide(this.amount,6, RoundingMode.HALF_EVEN);
     }
 }
